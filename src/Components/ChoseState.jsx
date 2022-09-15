c7710e8082929ffbf240f56e7d1fbe37dcab3260
drew <drewbeaven405@gmail.com>
2022-09-15 14:28:49 -0500

    use state in call

--- src/Components/ChoseState.jsx
+++ src/Components/ChoseState.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { states } from "./states";
 
-function ChoseState({setState},{state}) {
+function ChoseState({setState, state}) {
 
   return (
     <div className="App">
@@ -10,13 +11,17 @@ function ChoseState({setState},{state}) {
          }}
         >
 
-        <option Value="Missouri"> Missouri </option>
-        <option Value="Illinois"> Illinois </option>
-        <option Value="Colorado"> Colorado </option>
-        <option Value="Hell"> Hell </option>
+         {
+             states.map (data => (
+           <option  value={data.abbreviation}  key={data.abbreviation}> {data.name} </option>
+             ))
+         }
         </select>
     </div>
   );
 }
 
 export default ChoseState;
+
+
+
--- src/Components/StatePage.jsx
+++ src/Components/StatePage.jsx
@@ -1,8 +1,61 @@
 import React from 'react';
+import {useState} from 'react'
 
 
 export default function StatePage({state}) {
-    return(
-        <p>welcome to {state}</p>
-    )
-}
+  const [data, setData] = React.useState([]);
+  const [searchTerm, setSearchTerm] = useState('')
+  React.useEffect(() => {
+    let url = `https://developer.nps.gov/api/v1/parks?StateCode=` + {state} + `&limit=469&api_key=iemcdp722ZKWNmS5oMOwf64LiOd3fw6XSsq9tzUf`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.data);
+      })
+      .catch((error) => console.log(error));
+  }, [searchTerm]);
+
+
+     function handleSearch(e) {
+            e.preventDefault();
+            setSearchTerm({state})
+            }
+
+    if (data.length > 0) {
+    data.filter((i)  => {
+        return i.fullName.match(data);
+    });
+    }
+
+
+    function filterParkNames(park) {
+        if (searchTerm === ""){
+            return park
+        } if (park.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return park
+       }
+    }
+
+return (
+
+     <div>
+
+           {data.filter((park)=> {
+             return filterParkNames(park)
+           })
+           .map((park, key) => {
+             return (
+                 <div key={key}>
+                 <h2>{park.fullName}</h2>
+                 <br />
+                 {park.states}
+                 <br />
+                 {park.description}
+               </div>
+             )
+             })
+             }
+
+          </div>
+       );
+     };
