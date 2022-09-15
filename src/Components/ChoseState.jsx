effa45f2bcaf4e939d975d64c736a86b03c686c2
lucasbarth <lucasrbarth@gmail.com>
2022-09-15 14:59:08 -0500

    working

--- src/Components/ChoseState.jsx
+++ src/Components/ChoseState.jsx
@@ -2,21 +2,18 @@ import React from 'react'
 import { states } from "./states";
 
 function ChoseState({setState, state}) {
-
   return (
     <div className="App">
-        <select value={state} onChange={(e) => {
-            const selectedState = e.target.value;
-            setState(selectedState);
-         }}
-        >
-
-         {
-             states.map (data => (
-           <option  value={data.abbreviation}  key={data.abbreviation}> {data.name} </option>
-             ))
-         }
-        </select>
+      <select value={state} onChange={({ target: { value }}) => {
+        setState(value);
+      }}
+    >
+      {
+        states.map (data => (
+          <option  value={data.abbreviation}  key={data.abbreviation}> {data.name} </option>
+        ))
+      }
+      </select>
     </div>
   );
 }
--- src/Components/StatePage.jsx
+++ src/Components/StatePage.jsx
@@ -1,61 +1,56 @@
-import React from 'react';
-import {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-export default function StatePage({state}) {
-  const [data, setData] = React.useState([]);
-  const [searchTerm, setSearchTerm] = useState('')
-  React.useEffect(() => {
-    let url = `https://developer.nps.gov/api/v1/parks?StateCode=` + {state} + `&limit=469&api_key=iemcdp722ZKWNmS5oMOwf64LiOd3fw6XSsq9tzUf`;
+const StatePage = ({ state }) => {
+  const [ data, setData ] = useState([]);
+  // const [ searchTerm, setSearchTerm ] = useState('')
+
+  useEffect(() => {
+    let url = `https://developer.nps.gov/api/v1/parks?StateCode=${state}&limit=469&api_key=iemcdp722ZKWNmS5oMOwf64LiOd3fw6XSsq9tzUf`
     fetch(url)
       .then((response) => response.json())
       .then((json) => {
         setData(json.data);
       })
       .catch((error) => console.log(error));
-  }, [searchTerm]);
+  }, [ state ]);
 
 
-     function handleSearch(e) {
-            e.preventDefault();
-            setSearchTerm({state})
-            }
+  // function handleSearch(e) {
+  //   e.preventDefault();
+  //   setSearchTerm({state})
+  // }
 
-    if (data.length > 0) {
-    data.filter((i)  => {
-        return i.fullName.match(data);
-    });
-    }
+  // if (data.length > 0) {
+  //   data.filter((i)  => i.fullName.match(data));
+  // }
 
+  // function filterParkNames(park) {
+  //   if (searchTerm === ""){
+  //     return park
+  //   } if (park.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
+  //     return park
+  //   }
+  // }
 
-    function filterParkNames(park) {
-        if (searchTerm === ""){
-            return park
-        } if (park.fullName.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return park
-       }
-    }
+  return (
+    <div>
+      {state ? data
+        // .filter(park => filterParkNames(park))
+        .map((park, key) => {
+          return (
+            <div key={key}>
+              <h2>{park.fullName}</h2>
+              <br />
+              {park.states}
+              <br />
+              {park.description}
+            </div>
+          )
+        }) : <h2>Please select a state</h2>
+      }
+    </div>
+  );
+};
 
-return (
-
-     <div>
-
-           {data.filter((park)=> {
-             return filterParkNames(park)
-           })
-           .map((park, key) => {
-             return (
-                 <div key={key}>
-                 <h2>{park.fullName}</h2>
-                 <br />
-                 {park.states}
-                 <br />
-                 {park.description}
-               </div>
-             )
-             })
-             }
-
-          </div>
-       );
-     };
+export default StatePage
